f778e2f45abe24aa612f167dc00dc1c8d4fed148
LuckyL <lucderoos@gmail.com>
2019-03-21 09:11:11 +0100

    Create MiDownlight.js
    Added Xiaomi/Philips zhirui downlight node

---
+++ nodes/MiDownlight.js
@@ -0,0 +1,37 @@
+const packageFile = require("../package.json");
+
+const CommonDevice = require('./CommonDevice');
+const CommonNode = require('./CommonNode');
+
+module.exports = function(RED) {
+
+    class MiDownlightDevice extends CommonDevice {
+        constructor(config) {
+            super(RED, config);
+        }
+        
+        getAttributeList() {
+            return [
+                "power",
+                "bright",
+				"cct"
+            ];
+        }
+        
+        getAttributeSetter() {
+            return {
+                "power": "set_power",
+                "bright": "set_bright",
+				"cct":	"set_cct"
+            };
+        }
+    };
+    RED.nodes.registerType("Mi-Downlight-Device", MiDownlightDevice);
+    
+    class MiMiIntelligencePinboard extends CommonNode {
+        constructor(config) {
+            super(RED, config);
+        }
+    }
+    RED.nodes.registerType("Mi-Downlight", MiMiIntelligencePinboard);
+}
